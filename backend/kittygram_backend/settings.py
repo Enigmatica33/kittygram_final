2c43debfcea2a7dfcad43c8dc2bed0d852dd8e1b
Kristina Alabova <kristina.alabova@mail.ru>
2025-05-07 00:01:34 +0300

    add DEBUG in settings.py

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -9,10 +9,9 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
 SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 
-DEBUG = False
+DEBUG = (os.getenv('DEBUG', 'False') == 'True')
 
 ALLOWED_HOSTS = ['84.201.162.94', '127.0.0.1', 'localhost', 'kittygram.myftp.org']
 
