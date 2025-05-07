8fb7ce5778c7f4ddc8a9df3e104ee5a982c5a50d
Kristina Alabova <kristina.alabova@mail.ru>
2025-05-07 08:55:45 +0300

    Add getenv ALLOW_HOSTS in settings.py

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 
 DEBUG = (os.getenv('DEBUG', 'False') == 'True')
 
-ALLOWED_HOSTS = ['84.201.162.94', '127.0.0.1', 'localhost', 'kittygram.myftp.org']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(' ')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
